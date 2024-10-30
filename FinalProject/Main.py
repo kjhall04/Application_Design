8c048651f3c993edd50e0899715118b96edcf0a3
kjhall04 <hallkj04@gmail.com>
2024-10-29 19:36:20 -0500

    Final Project beginnings - database creator

---
+++ FinalProject/Main.py
@@ -0,0 +1,8 @@
+#! pyhton3
+# Main code to run the Equipment Logging Program
+
+import TestDatabase
+
+if __name__ == '__main__':
+    
+    TestDatabase.create_database()
---
+++ FinalProject/TestDatabase.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+def create_database():
+    # connect to sqlite database if on doesn't exist
+    conn = sqlite3.connect('EquipmetLogs.db')
+
+    # create cursor object
+    cursor = conn.cursor()
+
+    # create query for Contacts, Equipment, and Login Info
+    create_tables_query = '''
+    CREATE TABLE IF NOT EXISTS contact (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Fname TEXT NOT NULL,
+        Lname TEXT NOT NULL,
+        PhoneNumber TEXT NOT NULL,
+        Email TEXT NOT NULL
+    );
+    
+    CREATE TABLE IF NOT EXISTS equipment (
+        id INTEGER NOT NULL,
+        Ename TEXT NOT NULL,
+        DateInstalled TEXT NOT NULL,
+        Decomissioned BOOLEAN NOT NULL,
+        DecomissionedDate TEXT NOT NULL,
+        EquipmentAge TEXT NOT NULL,
+        MaintenanceDate TEXT NOT NULL,
+        Department TEXT NOT NULL
+    );
+    
+    CREATE TABLE IF NOT EXISTS login_data (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Username TEXT NOT NULL,
+        Password TEXT NOT NULL
+    );
+    '''
+
+    # Execute table creation query
+    cursor.executescript(create_tables_query)
+
+    # Commit changes to database
+    conn.commit()
+
+    # Exit database
+    conn.close()
+
+    print('Equipment database created successfully.')
